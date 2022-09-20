d86f86b13736cdc56835244e6e0871eb98eae892
chaitanyakrishna26399 <81625603+chaitanyakrishna26399@users.noreply.github.com>
2022-09-20 09:59:12 +0530

    Update sendGrid.js

--- config/sendGrid.js
+++ config/sendGrid.js
@@ -1,11 +1,11 @@
 const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey("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");
+sgMail.setApiKey("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");
 
 function otp (tomail, fourDigitOTP) {
 
     const msg = {
         "to": tomail,
-        "from": "hello@farcostore.com",
+        "from": "chaituchowdary040@gmail.com",
         "subject": 'assignment  otp',
         "text": `your one time otp is ${fourDigitOTP}`,
     }
