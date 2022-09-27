e5022fd0ed952c2d8a154c9599cf79361a70d63a
chaitanyakrishna26399 <81625603+chaitanyakrishna26399@users.noreply.github.com>
2022-09-27 10:34:54 +0530

    Update joi.js

--- config/joi.js
+++ config/joi.js
@@ -95,7 +95,6 @@ class userValidations {
     static async addTask(req, res, next) {
         // validate the input body value. 
         const validation = joi.object({
-            user_id: joi.number().integer().required(),
             date:joi.date().required(),
             task: joi.string().required(),
             status : joi.string().valid('completed','incompleted').required()
@@ -162,36 +161,6 @@ class userValidations {
             task_date:joi.date(),
             task: joi.string(),
             status : joi.string().valid('completed','incompleted')
-        })
-
-
-
-        //  below options are used to remove the qoutes in the validation response.
-        const options = {
-            errors: {
-                wrap: {
-                    label: ''
-                }
-            }
-        };
-
-
-        const err = validation.validate(req.body, options);
-        if (err.error) {
-            //return error message.
-            res.status(200).json({
-                status: false,
-                message: err.error.details[0].message
-            });
-        } else {
-            //moves to next method.
-            next();
-        }
-    }
-    static async getUserTask(req, res, next) {
-        // validate the input body value. 
-        const validation = joi.object({
-            user_id: joi.number().integer().required()
         })
 
 
