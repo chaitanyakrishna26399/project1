a818d1fef4ae09cd35a6fdada1ed33b7c2654ac7
chaitanyakrishna26399 <81625603+chaitanyakrishna26399@users.noreply.github.com>
2022-09-27 10:37:36 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -11,8 +11,8 @@ router.get('/', function(req, res, next) {
 router.post('/verifyEmail',joi.verifyMobile,userModule.verifyEmail)
 router.post('/userSignup',joi.signup,userModule.userSignup)
 router.get('/userLogin',joi.login,userModule.userLogin)
-router.post('/addTask',joi.addTask,userModule.addTaskS)
+router.post('/addTask',jwt.verifyToken,joi.addTask,userModule.addTaskS)
 router.patch('/updateTask',jwt.verifyToken,joi.updateTask,userModule.updateTask)
 router.delete('/deleteTask',jwt.verifyToken,joi.deleteTask,userModule.deleteTask)
-router.get('/getUserTask',jwt.verifyToken,joi.getUserTask,userModule.getUserTask)
+router.get('/getUserTask',jwt.verifyToken,userModule.getUserTask)
 module.exports = router;
